e3b9a375bce218ccd023e99dcde1f280cab88664
Farookbasha <159773076+Farook269@users.noreply.github.com>
2025-08-07 13:50:59 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import validator from 'validator'
 import userModel from '../models/userModel.js'
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken'
 import { v2 as cloudinary } from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
